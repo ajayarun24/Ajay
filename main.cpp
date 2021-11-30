57255e8328dc31884c69b1b29bab72cde1662409
Sarthak Singh <singhsarthak09@gmail.com>
2021-11-30 16:18:37 -0600

    Adding comments to main

--- main.cpp
+++ main.cpp
@@ -9,15 +9,9 @@
 using ::Parsing;
 
 int main() {
-    Parsing myParse ;
-    myParse.fillVector("data/CS225 final project data.csv");
-    myParse.fillNeighboringStates("data/neighbors-states.csv") ;
-
-
+    //Make a graph
     RoadTripGraph graph("data/CS225 final project data.csv", "data/neighbors-states.csv");
-
     graph.createGraph();
-
 
     return 0;
 }
