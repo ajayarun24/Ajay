ecde9f8be4d964dabe60375849aa6941aa87af28
Sarthak Singh <singhsarthak09@gmail.com>
2021-12-07 23:57:29 -0600

    Adding stuff

---
+++ .objs/tests/testingAlgorithms.d
@@ -0,0 +1,9 @@
+.objs/tests/testingAlgorithms.o: tests/testingAlgorithms.cpp \
+  tests/../cs225/catch/catch.hpp tests/../parsing.h \
+  tests/../roadtripgraph.h
+
+tests/../cs225/catch/catch.hpp:
+
+tests/../parsing.h:
+
+tests/../roadtripgraph.h:
--- roadtripgraph.d
+++
@@ -1,7 +0,0 @@
-roadtripgraph.o: roadtripgraph.cpp roadtripgraph.h parsing.h calculator.h
-
-roadtripgraph.h:
-
-parsing.h:
-
-calculator.h:
