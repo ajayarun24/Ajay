bd0bc48527bff45fa2a28c9fb3314a655b73c5fb
jaylenp <63690790+jaylenp@users.noreply.github.com>
2021-12-09 11:35:24 -0600

    map working

--- tests/testingAlgorithms.cpp
+++ tests/testingAlgorithms.cpp
@@ -13,22 +13,29 @@ TEST_CASE("Run input code", "[fileio"){
      RoadTripGraph graph("data/CS225 final project data.csv", "data/neighbors-states.csv");
      graph.createGraph();
      vector<Parsing::Location> i = graph.KruskalsMST();
-     graph.printBFS(14, 25);
+     vector<Parsing::Location> bfs = graph.BFS(0,4);
+     graph.printBFS(0, 4);
      graph.printDijkstra(1, 14);
-    //  PNG hi;
-    //  hi.readFromFile("../us_map.png");
-    //  std::cout << hi.width() << std::endl;
-    //  RoadTripGraph::addLines(hi, i);
+     PNG hi;
+     hi.readFromFile("../us_map.png");
+     PNG hi1;
+     hi1.readFromFile("../us_map1.png");
+    RoadTripGraph::addPoints(hi, bfs);
+    RoadTripGraph::addLines(hi1, bfs);
+
 
 }
 
 TEST_CASE("Test coordToPixel", "[fileio"){
      std::cout << Calculator::coordToPixel(119.54, 37.87).first << std::endl;
+     std::cout << Calculator::coordToPixel(119.54, 37.87).first + 1 << std::endl;
      std::cout << Calculator::coordToPixel(119.54, 37.87).second << std::endl;
      std::cout << Calculator::coordToPixel(87.62, 41.90).first << std::endl;
      std::cout << Calculator::coordToPixel(87.62, 41.90).second << std::endl;
      std::cout << Calculator::coordToPixel(80.13, 25.78).first << std::endl;
      std::cout << Calculator::coordToPixel(80.13, 25.78).second << std::endl;
+     std::cout << Calculator::coordToPixel(103.33, 41.70).first << std::endl;
+     std::cout << Calculator::coordToPixel(103.33, 41.70).second << std::endl;
 
 
 }
